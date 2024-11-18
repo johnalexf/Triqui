3f26ba7db035e63645d077b8a43e1ab9cc46b216
John Forero <johnalexfr@gmail.com>
2024-11-18 15:50:33 -0500

    creando funcion de pintarYGuardarJugada

--- script.js
+++ script.js
@@ -12,40 +12,100 @@ const contenedoresSecundarios = document.getElementsByClassName('secundario');
 let jugando = false;
 
 //variable de selección del usuario
-let letraSeleccionada = "Ninguna";
+let letraUsuario = "Ninguna";
+let letraPc = "Ninguna";
+let jugada = 0;
+
+//variable matriz del tablero de juego
+let matrizJuego = [
+    ["0","0","0"],
+    ["0","0","0"],
+    ["0","0","0"]
+];
+// La ubicación dentro del juego de cada div esta marcada con un id
+// de tal forma que la anterior variable seria el equivalente a la siguiente
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8]
 
 
+let columna = 0;
+let fila = 0;
+
+//variable para el contenedor que selecciona el usuario o el pc para pintar jugada
+let contenedorSeleccionado
+
+//arreglo para la primera jugada si es usuario elije la posición 4 que es la central
+let arregloPrimeraJugada = [0,2,6,8];
+
+let casillasOcupadas = [];
+
+//Eventos de escucha para que al seleccionar un checkbox el otro se deseleccione
+// y asignación de la elección en la variable letraSeleccion
 eleccionO.addEventListener('change',()=>{
     eleccionX.checked = false;
-    letraSeleccionada = "O";
+    letraUsuario = "O";
+    letraPc = "X";
 });
-
 
 eleccionX.addEventListener('change',()=>{
     eleccionO.checked = false;
-    letraSeleccionada = "X";
+    letraUsuario = "X";
+    letraPc = "O";
 });
 
 
-
-
 //evento de escucha en toda la pagina con el fin de:
-// poder dibujar la selección del usuario segun el contenedor secundario
+// poder dibujar la selección del usuario según el contenedor secundario donde se haga click.
 document.addEventListener('click',function(event){
-    console.log(event);
-    console.log(event.target.id);
-    console.log(event.target.classList[1]);
-    console.log(eleccionO.checked)
-    console.log(eleccionX.checked)
 
-    if(event.target.classList[1] == 'secundario' && letraSeleccionada != 'Ninguna'){
+    if(event.target.classList[1] == 'secundario' && letraUsuario != 'Ninguna'){
         if(jugando == false){
             jugando == true;
             eleccionO.disabled = true;
             eleccionX.disabled = true;
         }
-        contenedoresSecundarios[event.target.id - 1].innerHTML = `<p> ${letraSeleccionada} </p>`;
+        
+        contenedorSeleccionado = parseInt(event.target.id);
+        pintarYGuardarJugada(contenedorSeleccionado,letraUsuario);
+        
+        switch(jugada){
+            case 1:
+                if(contenedorSeleccionado == 4){
+                    contenedorSeleccionado = arregloPrimeraJugada[Math.round(Math.random() * 3)];
+                }else{
+                    contenedorSeleccionado = 4;
+                }
+                pintarYGuardarJugada(contenedorSeleccionado,letraPc);
+                break;
+            case 9:
+                jugando == false;
+                console.log("juego terminado")
+                break;
+            default:
+                do{
+                    contenedorSeleccionado = Math.round(Math.random() * 8);
+                    console.log(casillasOcupadas.indexOf(contenedorSeleccionado));
+                }while(casillasOcupadas.indexOf(contenedorSeleccionado)!=-1);
+                pintarYGuardarJugada(contenedorSeleccionado,letraPc);
+                break;
+
+        }
+        
+        
     }
     
 
 });
+
+function pintarYGuardarJugada(posicion, letra){
+    contenedoresSecundarios[posicion].innerHTML = `<p> ${letra} </p>`;
+
+    columna = Math.floor(posicion / 3);
+    fila = posicion % 3;
+    matrizJuego[columna][fila] = letra;
+    casillasOcupadas.push(posicion);
+    jugada++;
+    console.log(`La jugada es la # ${jugada}`);
+    console.log(matrizJuego);
+}
